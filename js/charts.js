15529646a9ac512ab60581f2d6afb64e078402bc
Reeon Saldanha <rsal0030@student.monash.edu>
2026-05-18 19:47:36 +1000

    Added sections

---
+++ js/charts.js
@@ -0,0 +1,8 @@
+vegaEmbed("#vis1", "vis/chart1.json");
+vegaEmbed("#vis2", "vis/chart2.json");
+
+const spaceTheme = {
+  background: "#0b1020",
+  titleColor: "#ffffff",
+  axisColor: "#cccccc",
+}
