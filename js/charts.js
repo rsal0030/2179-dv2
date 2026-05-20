258cba232e57db22c80c172f9fde49c2ecd825da
Reeon Saldanha <rsal0030@student.monash.edu>
2026-05-20 11:59:20 +1000

    Changed to csv files

--- js/charts.js
+++ js/charts.js
@@ -1,8 +1,12 @@
-vegaEmbed("#vis1", "vis/chart1.json");
-vegaEmbed("#vis2", "vis/chart2.json");
+// vegaEmbed("#vis1", "vis/chart1.json");
+// vegaEmbed("#vis2", "vis/chart2.json");
 
-const spaceTheme = {
-  background: "#0b1020",
-  titleColor: "#ffffff",
-  axisColor: "#cccccc",
-}
+// const spaceTheme = {
+//   background: "#0b1020",
+//   titleColor: "#ffffff",
+//   axisColor: "#cccccc",
+// }
+
+vegaEmbed("#chart1", "vis/chart1.json");
+vegaEmbed("#chart2", "vis/chart2.json");
+vegaEmbed("#chart3", "vis/chart3.json");
