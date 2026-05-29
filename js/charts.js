a30d35736fde89bc627df8c6e0018270d39b5ab5
Reeon Saldanha <rsal0030@student.monash.edu>
2026-05-29 18:22:35 +1000

    he

--- js/charts.js
+++ js/charts.js
@@ -8,9 +8,6 @@ const options = {
 };
 
 
-// ========================================
-// Section 1 — Australia Enters the Space Race
-// ========================================
 
 vegaEmbed("#donechart1", "vis/donechart1.json", options)
     .catch(console.error);
@@ -18,23 +15,22 @@ vegaEmbed("#donechart1", "vis/donechart1.json", options)
 vegaEmbed("#donechart2", "vis/donechart2.json", options)
     .catch(console.error);
 
-
-// ========================================
-// Section 2 — Industry Growth Accelerates
-// ========================================
-
 vegaEmbed("#donechart3", "vis/donechart3.json", options)
     .catch(console.error);
 
 vegaEmbed("#donechart4", "vis/donechart4.json", options)
     .catch(console.error);
 
-
-// ========================================
-// Section 3 — Australia Builds Satellite Capability
-// ========================================
-
 vegaEmbed("#donechart5", "vis/donechart5.json", options)
+    .catch(console.error);
+
+vegaEmbed("#donecharta", "vis/donecharta.json", options)
+    .catch(console.error);
+
+vegaEmbed("#donechartc", "vis/donechartc.json", options)
+    .catch(console.error);
+
+vegaEmbed("#donechartd", "vis/donechartd.json", options)
     .catch(console.error);
 
 vegaEmbed("#donechart6", "vis/donechart6.json", options)
@@ -46,12 +42,8 @@ vegaEmbed("#donechart7", "vis/donechart7.json", options)
 vegaEmbed("#donechart8", "vis/donechart8.json", options)
     .catch(console.error);
 
-
-// ========================================
-// Section 4 — The Next Frontier
-// ========================================
-
 vegaEmbed("#donechart9", "vis/donechart9.json", options)
     .catch(console.error);
 
-// vegaEmbed("#donechart10", "vis/donechart10.json", options);
+vegaEmbed("#donechartb", "vis/donechartb.json", options)
+    .catch(console.error);
